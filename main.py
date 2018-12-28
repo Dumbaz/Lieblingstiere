bd5bb3f1bcf7d163221d8bdbf08c8c8e2cc11505
Dorian Lenzner <iwantmyfingerback@gmail.com>
2018-12-28 21:30:28 +0100

    add code to create rectangle inside the svg

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import svgwrite
 
 svg_document = svgwrite.Drawing(filename= "test.svg", size = ("800px", "600px"))
 
+svg_document.add(svg_document.rect(
+  insert = (0,0),
+  size = ("200px", "100px"),
+  stroke_width = "1",
+  stroke = "black",
+  fill = "rgb(255,255,0)"))
+
 svg_document.save()
