1131e6469ecd11a7247c05d137b35ec08780fddf
Dorian Lenzner <iwantmyfingerback@gmail.com>
2018-12-28 21:42:11 +0100

    add code to merge 2 svg files and resulting image

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import svgwrite
+import svgutils
 
 svg_document = svgwrite.Drawing(filename= "test.svg", size = ("800px", "600px"))
 
@@ -10,3 +11,10 @@ svg_document.add(svg_document.rect(
   fill = "rgb(255,255,0)"))
 
 svg_document.save()
+
+first_svg = svgutils.transform.fromfile('spiders/spider.svg')
+second_svg = svgutils.transform.fromfile('bulls/bull-silhouette.svg')
+
+first_svg.append(second_svg)
+
+first_svg.save('merged.svg')
